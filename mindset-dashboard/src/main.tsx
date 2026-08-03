bd843edcf8744c53e8af7f6e554698cdd46eecff
Yannis <mindoraappli@gmail.com>
2026-08-03 13:29:12 +0200

    chore: add vercel analytics

--- mindset-dashboard/src/main.tsx
+++ mindset-dashboard/src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
+      <Analytics />
     </ErrorBoundary>
   </StrictMode>,
 )
